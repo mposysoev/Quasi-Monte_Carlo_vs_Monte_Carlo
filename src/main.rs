56bbd96866bd9eca28f8b1a8093c50f0707b3f62
Maksim Posysoev <37895103+mposysoev@users.noreply.github.com>
2022-12-17 19:45:07 +0100

    add results

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,54 @@ use rand::prelude::*;
 use sobol::params::JoeKuoD6;
 use sobol::Sobol;
 
-fn run_mk(N: usize) -> f64 {
+fn run_mk(n: usize) -> f64 {
     let mut rng = thread_rng();
 
-    let mut counter = 0;
-    for _ in 0..N {
+    let mut counter: f64 = 0.0;
+    for _ in 0..n {
         let x: f64 = rng.gen();
         let y: f64 = rng.gen();
 
         if x * x + y * y < 1.0 {
-            counter += 1;
+            counter += 1.0;
         }
     }
 
-    return 4.0 * (counter as f64) / (N as f64);
+    4.0 * counter / (n as f64)
 }
 
-fn run_qmk(N: usize) -> f64 {
+fn run_qmk(n: usize) -> f64 {
     let params = JoeKuoD6::minimal();
     let seq = Sobol::<f64>::new(2, &params);
 
-    let mut counter = 0;
-    for point in seq.take(N) {
+    let mut counter: f64 = 0.0;
+    for point in seq.take(n) {
         let x = point[0];
         let y = point[1];
 
         if x * x + y * y < 1.0 {
-            counter += 1;
+            counter += 1.0;
         }
     }
 
-    return 4.0 * (counter as f64) / (N as f64);
+    4.0 * counter / (n as f64)
 }
 
 fn main() {
-
-    println!("===============================");
-    println!("===   Обычный Монте-Карло   ===");
-    println!("===============================");
-    for i in 1..11 { 
-        let pow = (10 as i32).pow(i as u32) as usize; 
+    println!("==============================");
+    println!("===      Monte Carlo       ===");
+    println!("==============================");
+    for i in 1..11 {
+        let pow = (10_i32).pow(i as u32) as usize;
         println!("N = 10^{}:  Pi = {}", i, run_mk(pow))
     }
 
     println!("\n");
-    println!("==============================");
-    println!("===   Квази  Монте-Карло   ===");
-    println!("==============================");
-    for i in 1..11 { 
-        let pow = (10 as i32).pow(i as u32) as usize; 
+    println!("=============================");
+    println!("===   Quasi-Monte Carlo   ===");
+    println!("=============================");
+    for i in 1..11 {
+        let pow = (10_i32).pow(i as u32) as usize;
         println!("N = 10^{}:  Pi = {}", i, run_qmk(pow))
     }
-
 }
